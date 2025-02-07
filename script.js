7ac12c7db23c507a536d640803f6177b35cbec80
derek-m-martin <derekmartin1005@gmail.com>
2025-02-07 15:57:39 -0800

    Made the landing page have a typewriter-esque part

---
+++ script.js
@@ -0,0 +1,36 @@
+const typedTextSpan = document.querySelector(".typed-text");
+const cursor = document.querySelector(".cursor");
+
+const words = ["Swift", "Java", "Python", "C#", "JavaScript", "HTML/CSS"];
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+function type() {
+    const currentWord = words[wordIndex];
+    
+    if (isDeleting) {
+        typedTextSpan.textContent = currentWord.substring(0, charIndex - 1);
+        charIndex--;
+    } else {
+        typedTextSpan.textContent = currentWord.substring(0, charIndex + 1);
+        charIndex++;
+    }
+
+    let typeSpeed = isDeleting ? 100 : 200;
+
+    if (!isDeleting && charIndex === currentWord.length) {
+        typeSpeed = 2000;
+        isDeleting = true;
+    } else if (isDeleting && charIndex === 0) {
+        isDeleting = false;
+        wordIndex = (wordIndex + 1) % words.length;
+        typeSpeed = 500;
+    }
+
+    setTimeout(type, typeSpeed);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(type, 1000);
+});
